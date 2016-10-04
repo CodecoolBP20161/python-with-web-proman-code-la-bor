7ba3e36d9dd56801188a47885c13eb3b45c2d4d6
lorszil <lorszil@gmail.com>
2016-10-04 16:50:23 +0200

    to work on it

--- static/generalstorage.js
+++ static/generalstorage.js
@@ -1,1 +1,23 @@
-class GeneralStorage
+var myLocalStorage = function (){
+    this.saveBoard = function (){
+        prompt("save?");
+        console.log("save2?");
+    }
+    this.getBoard = function () {
+        prompt("get?");
+        console.log("get?2");
+    }
+};
+
+var GeneralStorage = function () {
+
+    var currentStorage = myLocalStorage()
+
+    genSaveBoard = function() {
+        currentStorage.saveBoard();
+    }
+
+    genGetBoard = function(){
+        currentStorage.getBoard();
+    }
+}
