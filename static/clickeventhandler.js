9943a4b53bfe5b123b075cb645e2912a08738ab8
eszterkaloz <eszterkaloz@gmail.com>
2016-10-05 17:22:43 +0200

    Nothing's changed. Adding again after it was deleted

--- static/clickeventhandler.js
+++ static/clickeventhandler.js
@@ -1,15 +1,15 @@
 var clickEventHandler = function(){
-    $("#button").click(function() {
-        var title = $("input[name=title]").val();
-        if (title === "") {
-            $("#alert").append("Please add a title");
-        }
-        else
-        {
-            $("#alert").remove();
-            var newBoard= createNewBoard(title);
-            generalStorage.currentStorage.saveBoard(newBoard);
-            DOMHandler.createDisplayableBoard(newBoard);
-        }
-    });
+   $("#button").click(function() {
+       var title = $("input[name=title]").val();
+       if (title === "") {
+           $("#alert").append("Please add a title");
+       }
+       else
+       {
+           $("#alert").remove();
+           var newBoard= createNewBoard(title);
+           generalStorage.currentStorage.saveBoard(newBoard);
+           DOMHandler.createDisplayableBoard(newBoard);
+       }
+   });
 };
