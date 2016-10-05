0d275fdc24ef54ace2650fe9967d6385a35c494d
berloc <berloc1987@gmail.com>
2016-10-05 14:37:27 +0200

    merged

--- static/clickeventhandler.js
+++ static/clickeventhandler.js
@@ -3,8 +3,10 @@ var clickEventHandler = function(){
         var title = $("input[name=title]").val();
         if (title == "") {
             $("#alert").append("Please add a title");
-        } else {
-            $("#alert").remove()
+        }
+        else 
+        {
+            $("#alert").remove();
             var newBoard= createNewBoard(title);
             myLocalStorage.saveBoard(newBoard);
             DOMHandler.showBoard(newBoard);
