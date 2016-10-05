b23195c1ee87e24f8dc5aec693e219e920ad32ff
berloc <berloc1987@gmail.com>
2016-10-05 16:49:49 +0200

    show/hide logic of the create new board box

--- static/main.js
+++ static/main.js
@@ -1,4 +1,13 @@
 // dom handling stuff (document...)
 $(document).ready(function(){
     clickEventHandler();
+     $('#board_create').hide();
+    $('#button_create_board').click(function() {
+        $('#def_board').hide();
+        $('#board_create').show();
+    });
+    $('#button_cancel').click(function() {
+        $('#board_create').hide();
+        $('#def_board').show();
+    })
 });
