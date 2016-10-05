a1fd790197e59b5eeac040d1899ce5be7effed18
eszterkaloz <eszterkaloz@gmail.com>
2016-10-05 12:12:57 +0200

    Implemented myLocalStorage getBoards method

--- static/mylocalstorage.js
+++ static/mylocalstorage.js
@@ -1,4 +1,16 @@
 var myLocalStorage = {
     saveBoard: function(board) {
-        localStorage.setItem( board.board_id, JSON.stringify(board) );
-}}
+        localStorage.setItem(board.board_id, JSON.stringify(board));
+    },
+
+    getBoards: function() {
+        var boardList = [],
+            boardIdKeys = Object.keys(localStorage),
+            allBoards = boardIdKeys.length;
+
+        while ( allBoards-- ) {
+            boardList.push(localStorage.getItem(boardIdKeys[allBoards]));
+        }
+        return boardList;
+    }
+}
