150f0c4a05a4ed61667264ce80f3f45fdc1b0774
eszterkaloz <eszterkaloz@gmail.com>
2016-10-05 12:12:18 +0200

    Communicating with the DOM through jQuery

--- static/clickeventhandler.js
+++ static/clickeventhandler.js
@@ -1,8 +1,9 @@
 var clickEventHandler = function(){
     $("#button").click(function() {
-        var title = document.getElementById("title").value;
+        // var title = document.getElementById("title").value;
+        var title = $("input[name=title]").val();
         var newBoard= createNewBoard(title);
         myLocalStorage.saveBoard(newBoard);
-        DOMHandler.showBoard(newBoard);
+        DOMHandler.showBoard();
     })
 };
