042957fd7478570f509c9f47f00ef9453f19e7fe
eszterkaloz <eszterkaloz@gmail.com>
2016-10-18 14:42:38 +0200

    $.ajax request to save new boards into the database

--- static/apihandler.js
+++ static/apihandler.js
@@ -1,12 +1,15 @@
 function APIHandler (){
 
-
-    this.saveBoard = function() {
-
-    }
+    this.saveBoard = function(board) {
+        $.ajax({
+            url: '/api/boards',
+            method: 'POST',
+            dataType: 'json',
+            data: JSON.stringify(board)
+        });
+    },
 
     this.getBoards = function() {
-
     }
 
 }
