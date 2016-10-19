5a9d1bb9ad2cca8f80370031fa11d3441d444b5a
eszterkaloz <eszterkaloz@gmail.com>
2016-10-19 15:22:12 +0200

    method getBoards() that is used to display them as well

--- static/apihandler.js
+++ static/apihandler.js
@@ -10,6 +10,20 @@ function APIHandler (){
     },
 
     this.getBoards = function() {
+        // var allBoards;
+        $.ajax({
+            url: 'api/boards',
+            method: 'GET',
+            // async: false,
+            success: function(allBoards){
+                JSON.parse(allBoards).forEach(function(element) {
+                    var title = element.title;
+                    DOMHandler.createDisplayableBoard(element, title);
+                })
+                // allBoards = allBoards;
+                },
+        })
+        // return allBoards;
     }
 
 }
