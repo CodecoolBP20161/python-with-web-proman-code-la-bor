e90bab8cf71d6de2c5511e34eaadcbe3fa3b4335
eszterkaloz <eszterkaloz@gmail.com>
2016-10-20 17:09:18 +0200

    Methods to save and get cards to and from server in comments

--- server.py
+++ server.py
@@ -37,5 +37,20 @@ def get_boards_from_database():
     return json.dumps(board_list)
 
 
+# @app.route('/api/<board_id>/cards', methods=['PUT'])
+# def save_card(board_id):
+#     for card in request.form:
+#         card_json = json.loads(card)
+#     # card_model = dict_to_model(Card, card_json)
+#     # card_model.save()
+#     return "a card has been saved"
+# 
+#
+# @app.route('/api/<board_id>/cards', methods=['GET'])
+# def get_cards_from_database():
+#     cards = Card.select().where(Card.board==board_id)
+#     card_list = [model_to_dict(card) for card in cards]
+#     return json.dumps(card_list)
+
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
